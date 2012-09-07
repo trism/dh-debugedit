d8035cb0d16bd4cf14ff4fb871ba9e263230ada3
Jason Conti <jason.conti@gmail.com>
2012-09-07 18:29:07 -0400

    * debugedit.mk:   - Use per-package build rules. Needed for packages with multiple -dbg     packages, like gtk+3.0. * dh_debugedit:   - Exit before attempting to install files if debugedit.list doesn't     exist.

--- debugedit.mk
+++ debugedit.mk
@@ -3,7 +3,7 @@
 CDBS_BUILD_DEPENDS_rules_debugedit := dh-debugedit
 CDBS_BUILD_DEPENDS += , $(CDBS_BUILD_DEPENDS_rules_debugedit)
 
-common-binary-post-install-arch:: debian/debugedit.after
+common-binary-post-install-arch:: $(patsubst %,binary-debugedit/%,$(DEB_ALL_PACKAGES))
 
-debian/debugedit.after:
-	dh_debugedit $(DEB_DH_DEBUGEDIT_ARGS)
+$(patsubst %,binary-debugedit/%,$(DEB_ALL_PACKAGES)):
+	dh_debugedit -p$(cdbs_curpkg) $(DEB_DH_DEBUGEDIT_ARGS)
